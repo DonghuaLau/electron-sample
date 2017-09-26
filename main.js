f26906f655896a3d2a607fe3b68ce91aafd6252e
Donghua.Lau <me@liudonghua.net>
2017-09-26 17:48:11 +0800

    update code

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+const jQuery = require('jquery')
 
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+			 width: 800
+			,height: 560
+			,minWidth: 800
+			,minHeight: 560
+			,resizable: true 
+			,frame: false
+		})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,8 +23,13 @@ function createWindow () {
     slashes: true
   }))
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+
+  //win = BrowserWindow.getFocusedWindow()
+
+
+  //win.webContents.openDevTools()
+
+
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -29,17 +39,29 @@ function createWindow () {
     win = null
   })
 
-
-//const contextBtn = document.getElementById('btn-imap')
-//contextBtn.addEventListener('click', function () {
-//	test_imap();
-//});
-
+  initEvents();
 
 }
 
+function initEvents() {
+	console.log("init events");
 
+	/*
+	jQuery("#close-win").click( function(){
+	  win.close();
+	});
+	*/
 
+	document.getElementById("close-win").onclick = function() {
+	  win.close();
+	}
+	document.getElementById("min-win").onclick = function() {
+	  win.minimize();
+	}
+	document.getElementById("max-win").onclick = function() {
+	  win.isMaximized() ? win.unmaximize() : win.maximize();
+	}
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -63,28 +85,5 @@ app.on('activate', () => {
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
-
-//app.on("#btn-imap", test_imap);
-
-
-
-
-
-
-
-
-
-
-/*
-jQuery(document).ready( function(){
-
-
-	jQuery("#btn-imap").click( function(){
-		//test_imap_2();
-		test_imap();
-	});
-});
-*/
+//initEvents();
